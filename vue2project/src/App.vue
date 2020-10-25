5b62153b798f77df28657ef2697682035218186d
timycodes <tcboulder@outlook.com>
2020-10-24 22:08:45 -0400

    beginning of the reconstruction of vue project
    Removed most everything inside the  HelloWorld.vue componet in the template. Added girl.jpg to  assests and applied flex in scoped style.

--- vue2project/src/App.vue
+++ vue2project/src/App.vue
@@ -1,28 +1,48 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<div class="girl-container">
+			<img class="face" alt="artwork of female face" src="./assets/girl.jpg" />
+		</div>
+
+		<HelloWorld msg="Tim Conroy | Frontend Developer" />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: 'App',
+	components: {
+		HelloWorld,
+	},
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: 'Roboto', sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+
+.girl-container {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	border: 2px solid black;
+	margin: auto;
+	background-color: black;
+}
+.face {
+	display: block;
+	width: 100%;
+	max-width: 360px;
+	height: auto;
+	margin: auto;
+	padding: 1.3rem;
 }
 </style>
